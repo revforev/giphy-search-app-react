3176b67837823f55c6119696abb9d07af99c7f3a
revforev <dutulescu_andrei@yahoo.com>
2024-02-06 18:15:54 +0000

    giphy search app added input, with the onHandleKey and onKeyPress events, passed the input value to the state and used the state to change things on the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+// here we import in our loader as an image
+import loader from './images/loader.svg';
+import './css/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+);
+
+const UserHint = ({loading, hintText}) => (
+  <div className="user-hint">
+    {/* here we check wheter we have a loading state and render out
+  either our spinner or hintText based on that, using a ternary
+  operator */}
+    {loading ? <img alt="loader" className="block mx-auto" src={loader} /> : hintText}
+  </div>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      hintText: 'Hit enter to search',
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event.key);
+
+    const {value} = event.target;
+    // by setting the searchTerm in our state and also
+    // using that on the input as the value, we have created
+    // what is called a controlled input
+    this.setState((prevState, props) => ({
+      // we take our old props and spread them out here
+      ...prevState,
+      // and then we overwrite the ones we want after
+      searchTerm: value,
+      // we set the hint text only when we have more then
+      // 2 characters in our input, otherwise it is an empty string
+      hintText: value.length >2 ? `Hit enter to search ${value}` : '',
+    }));
+
+    if (value.length > 2) {
+      console.log('this is a valid search term');
+    }
+  };
+
+  handleKeyPress = (event) => {
+    const {value} = event.target;
+    // when we have 2 or more characters in our search box
+    // and we have also pressed enter, we then want to run a search
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`search for ${value}`);
+    }
+
+    console.log(event.key);
+  };
+
+  render() {
+    const {searchTerm} = this.state;
+    return (
+      <div className="page">
+        <Header />
+        <div className="search grid">
+          {/* {stack of gif images} */}
+          <input
+            className="input grid-item"
+            placeholder="Type something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}
+          ></input>
+        </div>
+        <UserHint {...this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
