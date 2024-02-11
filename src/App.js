76dd6678bd13c92200a6065a7de236e55ef79372
revforev <dutulescu_andrei@yahoo.com>
2024-02-11 12:02:49 +0000

    input-focus-on-clearSearch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, {Component} from 'react';
 // here we import in our loader as an image
 import loader from './images/loader.svg';
 import './css/main.css';
+import Gif from './gif';
+import clearButton from './images/close-icon.svg';
 
 const randomChoice = (arr) => {
   const randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
 };
 
-const Header = () => (
+const Header = ({clearSearch, hasResults}) => (
   <div className="header grid">
-    <h1 className="title">Jiffy</h1>
+    {hasResults ? (
+      <button onClick={clearSearch}>
+        <img src={clearButton} />
+      </button>
+    ) : (
+      <h1 className="title">Jiffy</h1>
+    )}
   </div>
 );
 
@@ -26,11 +34,11 @@ const UserHint = ({loading, hintText}) => (
 class App extends Component {
   constructor(props) {
     super(props);
+    // this.textInput = React.createRef();
     this.state = {
+      loading: false,
       searchTerm: '',
-      hintText: 'Hit enter to search',
-      gif: null,
-      // we have an array of gifs
+      hintText: '',
       gifs: [],
     };
   }
@@ -43,6 +51,10 @@ class App extends Component {
   // that let us use the async/await style of function
 
   searchGiphy = async (searchTerm) => {
+    this.setState({
+      // we first set the loading state to be true
+      loading: true,
+    });
     try {
       const response = await fetch(
         `https://api.giphy.com/v1/gifs/search?api_key=YLaXcGQpJq0zBZcIINCx3INyMdrdAgcb&q=${searchTerm}&limit=25&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
@@ -51,6 +63,14 @@ class App extends Component {
       // here we convert our raw response into json data
       // const {data} gets the .data part of our repsponse
       const {data} = await response.json();
+
+      // here we check if the area of the results is empty
+      // if it is, we throw an error which will stop the code
+      // here while we catch it and handle it
+
+      if (!data.length) {
+        throw `Nothing found for ${searchTerm}`;
+      }
 
       // here we grab a random result from our images
       const randomGif = randomChoice(data);
@@ -64,8 +84,18 @@ class App extends Component {
         // here we use our spread to take the previous gifs
         // spread them out and then add a new gif onto the end
         gifs: [...prevState.gifs, randomGif],
+        // we turn off our loading spinner again
+        loading: false,
+        hintText: `Hit enter to see more ${searchTerm}`,
       }));
-    } catch (error) {}
+    } catch (error) {
+      this.setState((prevState, props) => ({
+        ...prevState,
+        hintText: error,
+        loading: false,
+      }));
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -102,18 +132,33 @@ class App extends Component {
     console.log(event.key);
   };
 
+  // here we reset our state by clearing everything up
+  // and making it default again
+
+  clearSearch = () => {
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: '',
+      hintText: '',
+      gifs: [],
+    }));
+    this.textInput.focus();
+  };
+
   render() {
-    const {searchTerm, gif} = this.state;
+    const {searchTerm, gifs} = this.state;
+    const hasResults = gifs.length;
+
     return (
       <div className="page">
-        <Header />
+        <Header clearSearch={this.clearSearch} hasResults={hasResults} />
         <div className="search grid">
           {/* {stack of gif images} */}
           {/* it's only going to render our video when we have a gif
           // in the state, we can test for it using && */}
 
           {this.state.gifs.map((gif) => (
-            <video className="grid-item video" autoPlay loop src={gif.images.original.mp4} />
+            <Gif {...gif} />
           ))}
 
           <input
@@ -122,6 +167,9 @@ class App extends Component {
             onChange={this.handleChange}
             onKeyPress={this.handleKeyPress}
             value={searchTerm}
+            ref={(input) => {
+              this.textInput = input;
+            }}
           ></input>
         </div>
         <UserHint {...this.state} />
